65e526757f793997c0f17bfbe8d23a2115b3b920
Matt Dambra <mpdambra@gmail.com>
2020-04-18 11:16:17 -0400

    reverting old site

--- src/components/Container.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Container(properties) {
-  return <div className={`container${properties.fluid ? "-fluid" : ""}`}>{properties.children}</div>;
-}
-
-export default Container;
